2f235707f09e63889cfbb0267f4f3845ff7bbd1a
Niklas Lausch <support@niki2k1.de>
2020-01-26 12:26:38 +0100

    Added Static Domain Test Data

--- app/Controllers/Http/DomainController.js
+++ app/Controllers/Http/DomainController.js
@@ -5,7 +5,40 @@ const sha1 = require('sha1');
 
 class DomainController {
     list({ request, response }) {
-
+        const domains = [
+            {
+                name: 'Frozencloud.de',
+                subdomains: [
+                    {
+                        name: 'jira.frozencloud.de',
+                        application: 'Atlassian Jira'
+                    }
+                ]
+            },
+            {
+                name: 'Niki2k1.de',
+                subdomains: [
+                    {
+                        name: 'test.niki2k1.de',
+                        application: 'Test Application'
+                    }
+                ]
+            },
+            {
+                name: 'Lightainer.de',
+                subdomains: [
+                    {
+                        name: 'marketplace.lightainer.de',
+                        application: 'Lightainer Marketplace'
+                    },
+                    {
+                        name: 'status.lightainer.de',
+                        application: 'Lightainer Status Page'
+                    }
+                ]
+            }
+        ]
+        response.status(200).send(JSON.stringify(domains))
     }
 }
 
